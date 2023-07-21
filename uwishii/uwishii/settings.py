62e183d6cfbe31e409fd6fe4e450d89cc9c6fe5f
Java-rice <johnmarkpperoche@iskolarngbayan.pup.edu.ph>
2023-07-21 20:31:00 +0800

    some changes in settings.py

--- uwishii/uwishii/settings.py
+++ uwishii/uwishii/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u!h87dnb4&3gj#+m2x7c2kc9ck08%x@%@ov_#hn93!^o+6irn8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
